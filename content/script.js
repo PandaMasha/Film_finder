f7cbfab7b6661820db25b0e673bb08cf1d7e2fcd
Panda development <92259451+PandaMasha@users.noreply.github.com>
2025-09-11 16:20:44 +0300

    Update script.js

--- content/script.js
+++ content/script.js
@@ -1,4 +1,4 @@
-const apiKey = secrets.<APIKEY>;
+const tmdbKey = "secrets.<APIKEY>";
 const tmdbBaseUrl = "https://api.themoviedb.org/3";
 const playBtn = document.getElementById("playBtn");
 let cachedGenres = null;
